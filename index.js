3d6729b1fa788237621a6d2c9d425ad39a10def8
Faizah Ahsan <faizah1992@gmail.com>
2021-04-01 22:37:12 -0500

    render emoji function  added

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ pushBtn.addEventListener("click", function(){
         renderEmojis()
     }
 })
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+        if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmojis()
+    }
+})
